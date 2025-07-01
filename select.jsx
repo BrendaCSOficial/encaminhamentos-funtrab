7a0da14e2ad4c3e8589bde432b7a218aae86864e
BrendaCSOficial <brendacostassouza@gmail.com>
2025-07-01 17:08:10 -0400

    Add files via upload

---
+++ select.jsx
@@ -0,0 +1,5 @@
+export function Select({ children }) { return <select>{children}</select>; }
+export function SelectTrigger({ children }) { return <div>{children}</div>; }
+export function SelectValue({ children }) { return <span>{children}</span>; }
+export function SelectContent({ children }) { return <div>{children}</div>; }
+export function SelectItem({ children }) { return <option>{children}</option>; }
